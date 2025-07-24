b99edf4b88966aed2dcce8682c18542d6883d00d
Avi Sharma <avisharma5016@gmail.com>
2025-07-24 09:55:48 +0530

    Star_pattern.cpp

---
+++ Star_pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        for (int a = 1; a <= i; ++a) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+*/output
+*
+**
+***
+****
+*****
+/*
