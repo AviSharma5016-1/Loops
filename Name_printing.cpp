15f143f2912f8bcd683c03148dd931540e718086
Avi Sharma <avisharma5016@gmail.com>
2025-07-31 10:04:30 +0530

    Name_printing.cpp

---
+++ Name_printing.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++)
+    {cout<<"Avi Sharma"<<endl;}
+    return 0;
+}
+
+/* OUTPUT
+Avi Sharma
+Avi Sharma
+Avi Sharma
+Avi Sharma
+Avi Sharma
+*/
